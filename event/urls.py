ac9936c4be5ed8e9a53bb38193191047991f35b8
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-03-07 08:45:14 +0000

    register, signin, signout, basic styling, change title and head/logo, connect home and about, js file for reviews, event_detail.html creation, add functions to event views

--- event/urls.py
+++ event/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.EventList.as_view(), name = 'home'),
+<<<<<<< HEAD
     path('<slug:slug>/', views.event_detail, name='event_detail'),
     path('<slug:slug>/edit_review/<int:review_id>',
          views.review_edit, name='review_edit'),
     path('<slug:slug>/delete_review/<int:review_id>',
          views.review_delete, name='review_delete'),
+=======
+>>>>>>> c96785c (register, signin, signout, basic styling, change title and head/logo, connect home and about, js file for reviews, event_detail.html creation, add functions to event views)
 ]
